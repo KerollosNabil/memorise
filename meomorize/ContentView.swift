a29c78fb76d916b20905bb14ac36178e77bb8dfd
KerollosNabil <kokonabi4444@gmail.com>
2020-06-18 22:11:00 +0200

    Initial Commit

---
+++ meomorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  meomorize
+//
+//  Created by MAC on 6/18/20.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
